06e191377b9ba8a4f15272f93cbfb27da9235b91
garicchi <xgaricchi@gmail.com>
2014-02-15 16:36:42 +0900

    FirstCommit

---
+++ RuntimeProxy/ProxySetting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.Security.Credentials.UI;
+
+namespace RuntimeProxy
+{
+    public static class ProxySetting
+    {
+        private static NetworkCredential Credential;
+        /// <summary>
+        /// プロキシ接続を構成します
+        /// </summary>
+        /// <param name="testUri">テスト接続用Uri</param>
+        public static async Task SetProxyAsync(Uri testUri)
+        {
+            var config = await NetworkInformation.GetProxyConfigurationAsync(testUri);
+            if (config.CanConnectDirectly==false)
+            {
+                NetworkCredential tempCredential = null;
+                if (Credential == null)
+                {
+                    CredentialPickerOptions credPickerOptions = new CredentialPickerOptions();
+                    credPickerOptions.Message = "認証情報を入力してください";
+                    credPickerOptions.Caption = "プロキシネットワークに接続します";
+                    credPickerOptions.TargetName = "target";
+                    credPickerOptions.AlwaysDisplayDialog = true;
+                    credPickerOptions.AuthenticationProtocol = AuthenticationProtocol.Basic;
+                    var pickResult = await CredentialPicker.PickAsync(credPickerOptions);
+
+                    tempCredential = new NetworkCredential(pickResult.CredentialUserName, pickResult.CredentialPassword);
+                    if (pickResult.CredentialSaved)
+                    {
+                        Credential = tempCredential;
+                    }
+                }
+                else 
+                {
+                    tempCredential = Credential;
+                }
+                
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.Proxy = WebRequest.DefaultWebProxy;
+                handler.Proxy.Credentials = tempCredential;
+                handler.UseProxy=true;
+            }
+            
+        }
+
+
+    }
+}
---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using RuntimeProxy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace TestApp
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //プロキシー設定
+            await ProxySetting.SetProxyAsync(new Uri("http://google.com"));
+
+
+            HttpClient client = new HttpClient();
+
+            var s = await client.GetStringAsync("http://google.com");
+            MessageDialog dialog = new MessageDialog(s);
+            await dialog.ShowAsync();
+            
+                
+        }
+    }
+}
